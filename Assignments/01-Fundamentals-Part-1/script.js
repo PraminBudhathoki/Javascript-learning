0ce99ed7eb56bc7aa6e559ac6d70063ae14ab73c
PraminBudhathoki <pramin2059@gmail.com>
2022-01-17 22:53:18 +0545

    files added 1st time

---
+++ Assignments/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,29 @@
+// Values and Variables
+
+// 1
+let countryName ="Nepal";
+let continentName ="Asia";
+let population = 29140000;
+
+// 2
+console.log(countryName);
+console.log(continentName);
+console.log(population);
+
+
+// Datatypes
+// 1
+let isIsland = false;
+let language;
+
+// 2
+console.log(isIsland);
+console.log(population);
+console.log(countryName);
+console.log(language);
+
+//let,var and const
+//1
+language = "Nepali";
+
+//2
