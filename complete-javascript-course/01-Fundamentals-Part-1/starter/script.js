5d5fdf53ad3fdc07e4c9b82e6e98dffff08fa4f7
PraminBudhathoki <pramin2059@gmail.com>
2022-01-18 15:05:08 +0545

    changes restored

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,98 @@
+// let js = 'amazing';
+// if(js==='amazing')alert("Javascript is fun!");
+// console.log(48+8+23-10);
+
+// console.log('Pramin');
+// console.log(23);
+
+// let firstName ="Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+//Variable name conventions
+// let jonas_matilda = "3M";
+// let $function =27;
+
+// let  person ="Pramin"
+// let PI = 3.1415;
+
+// let myFirstJob ="Programmer";
+// let myCurrentJob ="Teacher";
+
+// console.log(myFirstJob);
+
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Pramin');
+
+// javascriptIsFun ='Yes!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+// let age = 30;
+// age = 31;
+
+// const birthyear =1991;
+// birthyear =1990; --error
+
+// const job; --error--you can't just declare the variable using const you must also define it.
+
+// var job = 'Programmer';
+// job = "teacher"; 
+
+// lastName = 'Budhathoki';
+// console.log(lastName);
+
+////////////////////////////
+//Basic Operator
+//Math operator
+// const now =2037;
+// const agePramin = now -2002;
+// const agePush = now-2003;
+// console.log(agePramin , agePush);
+
+
+// console.log(agePramin*2 , agePramin/10, 2**3);
+//2**3 means 2 to the power 3 which is 2*2*2=8
+
+// const firstName ="Pramin";
+// const lastName ="Budhathoki";
+// console.log(firstName+' '+lastName);
+
+
+//Assigmment operator
+
+// let x =10 + 5;  //15
+// x+=10;// x =x+10 => 25
+// x*=4; // x=x*4 => 100
+// x++;  // x=x+1;
+// x--;
+// x--;
+// ++x;
+// --x;
+// console.log(x);
+
+//comparision operator
+// console.log(agePramin > agePush);//>,<,>=,<=
+// console.log(agePush >= 18);
+
+// const isFullAge =  agePush >= 18;
+
+// console.log(now -2002 > now-2003);
+ 
