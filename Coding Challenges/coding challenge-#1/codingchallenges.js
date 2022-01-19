e6e7e39c9cb9570d73959cbcac482aadf8327708
PraminBudhathoki <pramin2059@gmail.com>
2022-01-19 11:51:27 +0545

    coding challenge done by myself

---
+++ Coding Challenges/coding challenge-#1/codingchallenges.js
@@ -0,0 +1,30 @@
+let markMass , johnMass ;
+let markHeight ,johnHeight ;
+
+
+//Data-1
+
+markMass = 78;
+johnMass = 92;
+markHeight = 1.69;
+johnHeight = 1.95;
+let markBMI = markMass / markHeight**2;
+let johnBMI = johnMass / johnHeight**2;
+let markHigherBMI =markBMI > johnBMI;
+console.log(markHigherBMI);
+
+
+
+//Data-2
+
+markMass = 95;
+johnMass = 85;
+markHeight = 1.88;
+johnHeight = 1.76;
+markBMI = markMass / markHeight**2;
+johnBMI = johnMass / johnHeight**2;
+markHigherBMI =markBMI > johnBMI;
+console.log(markHigherBMI);
+
+
+
