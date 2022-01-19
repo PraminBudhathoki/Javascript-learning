a3b2c286d6887fe9bea78e85b217f5e21e44f291
PraminBudhathoki <pramin2059@gmail.com>
2022-01-19 18:47:04 +0545

    folder name changed and coding challenge 2 completed

---
+++ Coding Challenges/coding challenge--Fundamentals-Part-1/codingchallenges.js
@@ -0,0 +1,31 @@
+//coding challenge #1
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+//////////////////////////////////////
+//Coding Challenge - 2
+if(markHigherBMI){
+    console.log(`Mark's BMI is higher than John's!`);
+}else{
+    console.log(`John's BMI is higher than Mark's!`);
+}
+if(markHigherBMI){
+    console.log(`Mark's BMI(${BMIMark}) is higher than John's(${BMIJohn})!`);
+}else{
+    console.log(`John's BMI(${BMIJohn}) is higher than Mark's(${BMIMark})!`);
+}
