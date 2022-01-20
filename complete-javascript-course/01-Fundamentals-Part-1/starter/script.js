4c17cdc7643b507f2b588d316f60b43ea797a428
PraminBudhathoki <pramin2059@gmail.com>
2022-01-20 15:49:38 +0545

    Equality operator learned

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -190,4 +190,29 @@ if (height) {
   console.log('Height is UNDEFINED');
 }
 
+////////////////////////////////////////
+//Equality operator
+const age = 18;
+if(age  === 18) console.log("You just become adult(strict)");
+
+//=== is called strict equality operator which does not perform type coersion.
+//== is called loose equality operator which performs type coersion
+if(age  == 18) console.log("You just become adult(loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // 22 === 23 -> FALSE
+    console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+  
+if (favourite !== 23) console.log('Why not 23?');
+  
 
