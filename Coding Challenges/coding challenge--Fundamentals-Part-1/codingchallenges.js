7910dd96de000ca798a03ef2b9fec757cd07a070
PraminBudhathoki <pramin2059@gmail.com>
2022-01-21 17:25:31 +0545

    coding challenge by myself completed

--- Coding Challenges/coding challenge--Fundamentals-Part-1/codingchallenges.js
+++ Coding Challenges/coding challenge--Fundamentals-Part-1/codingchallenges.js
@@ -6,29 +6,51 @@
 // const heightJohn = 1.95;
 
 
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn, markHigherBMI);
+// console.log(BMIMark, BMIJohn, markHigherBMI);
 
-//////////////////////////////////////
-//Coding Challenge - 2
-if(markHigherBMI){
-    console.log(`Mark's BMI is higher than John's!`);
-}else{
-    console.log(`John's BMI is higher than Mark's!`);
-}
-if(markHigherBMI){
-    console.log(`Mark's BMI(${BMIMark}) is higher than John's(${BMIJohn})!`);
-}else{
-    console.log(`John's BMI(${BMIJohn}) is higher than Mark's(${BMIMark})!`);
-}
+// //////////////////////////////////////
+// //Coding Challenge - 2
+// if(markHigherBMI){
+//     console.log(`Mark's BMI is higher than John's!`);
+// }else{
+//     console.log(`John's BMI is higher than Mark's!`);
+// }
+// if(markHigherBMI){
+//     console.log(`Mark's BMI(${BMIMark}) is higher than John's(${BMIJohn})!`);
+// }else{
+//     console.log(`John's BMI(${BMIJohn}) is higher than Mark's(${BMIMark})!`);
+// }
 
 //////////////////////////////////////
 //Coding Challenge -3
+let dolphinScore1 = Number(prompt("Enter the 1st score of Dolphin:"));
+let dolphinScore2 = Number(prompt("Enter the 2nd score of Dolphin:"));
+let dolphinScore3 = Number(prompt("Enter the 3rd score of Dolphin:"));
+
+let koalasScore1 = Number(prompt("Enter the 1st score of Koalas:"));
+let koalasScore2 = Number(prompt("Enter the 2nd score of Koalas:"));
+let koalasScore3 = Number(prompt("Enter the 3rd score of Koalas:"));
+
+const averageDolphin = (dolphinScore1 + dolphinScore2 + dolphinScore3 )/3;
+const averageKoalas = (koalasScore1 + koalasScore2 + koalasScore3 )/3;
+if(averageDolphin > 100 || averageKoalas > 100){
+    
+    if(averageDolphin > averageKoalas){
+        console.log("Dolphin is the winner");
+        
+    }else if(averageKoalas > averageDolphin){
+        console.log("Koalas is the winner");
+    }else{
+        console.log("Both are draw");
+    
+    }
+}
--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -240,40 +240,71 @@
 //   console.log('Someone else should drive...');
 // }
 
-const   day  = prompt("Enter the day");
+// const   day  = prompt("Enter the day");
 
-switch(day){
-    case 'monday': //day === 'monday'
-        console.log('Plan for projects');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory lectures');
-        break;
-    case 'wednesday':
-    case 'thrusday':
-        console.log('Write code examples');
-    case 'friday':
-        console.log('Record videos');
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend :D');
-        break; 
-    default:
-        console.log('Not a valid day!');
+// switch(day){
+//     case 'monday': //day === 'monday'
+//         console.log('Plan for projects');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory lectures');
+//         break;
+//     case 'wednesday':
+//     case 'thrusday':
+//         console.log('Write code examples');
+//     case 'friday':
+//         console.log('Record videos');
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend :D');
+//         break; 
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+// if (day === 'monday') {
+//     console.log('Plan for projects');
+//     console.log('Go to coding meetup');
+//   } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+//   } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples');
+//   } else if (day === 'friday') {
+//     console.log('Record videos');
+//   } else if (day === 'saturday' || day === 'sunday') {
+//     console.log('Enjoy the weekend :D');
+//   } else {
+//     console.log('Not a valid day!');
+//   }
+
+////////////////////////////////////
+// Statements and Expressions
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+  const str = '23 is bigger';
 }
 
-if (day === 'monday') {
-    console.log('Plan for projects');
-    console.log('Go to coding meetup');
-  } else if (day === 'tuesday') {
-    console.log('Prepare theory videos');
-  } else if (day === 'wednesday' || day === 'thursday') {
-    console.log('Write code examples');
-  } else if (day === 'friday') {
-    console.log('Record videos');
-  } else if (day === 'saturday' || day === 'sunday') {
-    console.log('Enjoy the weekend :D');
-  } else {
-    console.log('Not a valid day!');
-  }
+const me = 'Pramin';
+console.log(`I'm ${2037 - 2002} years old ${me}`);
+
+//////////////////////////////////////
+//The ternary operator
+const age = 23;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
