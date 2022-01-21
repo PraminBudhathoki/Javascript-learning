02df0a9e52bfaa2cab3294c443a390dcf6cd77aa
PraminBudhathoki <pramin2059@gmail.com>
2022-01-21 10:41:11 +0545

    switch statement learned

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -240,3 +240,40 @@
 //   console.log('Someone else should drive...');
 // }
 
+const   day  = prompt("Enter the day");
+
+switch(day){
+    case 'monday': //day === 'monday'
+        console.log('Plan for projects');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory lectures');
+        break;
+    case 'wednesday':
+    case 'thrusday':
+        console.log('Write code examples');
+    case 'friday':
+        console.log('Record videos');
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break; 
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan for projects');
+    console.log('Go to coding meetup');
+  } else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+  } else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+  } else if (day === 'friday') {
+    console.log('Record videos');
+  } else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+  } else {
+    console.log('Not a valid day!');
+  }
