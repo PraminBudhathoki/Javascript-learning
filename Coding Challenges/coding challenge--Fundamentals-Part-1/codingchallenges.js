1296f8bbd8070d1f73edd141f0a4c3dbd250cd42
PraminBudhathoki <pramin2059@gmail.com>
2022-01-21 19:38:25 +0545

    Challenge of fundamentals part 1 completed

--- Coding Challenges/coding challenge--Fundamentals-Part-1/codingchallenges.js
+++ Coding Challenges/coding challenge--Fundamentals-Part-1/codingchallenges.js
@@ -32,25 +32,57 @@
 
 //////////////////////////////////////
 //Coding Challenge -3
-let dolphinScore1 = Number(prompt("Enter the 1st score of Dolphin:"));
-let dolphinScore2 = Number(prompt("Enter the 2nd score of Dolphin:"));
-let dolphinScore3 = Number(prompt("Enter the 3rd score of Dolphin:"));
+// let dolphinScore1 = Number(prompt("Enter the 1st score of Dolphin:"));
+// let dolphinScore2 = Number(prompt("Enter the 2nd score of Dolphin:"));
+// let dolphinScore3 = Number(prompt("Enter the 3rd score of Dolphin:"));
 
-let koalasScore1 = Number(prompt("Enter the 1st score of Koalas:"));
-let koalasScore2 = Number(prompt("Enter the 2nd score of Koalas:"));
-let koalasScore3 = Number(prompt("Enter the 3rd score of Koalas:"));
+// let koalasScore1 = Number(prompt("Enter the 1st score of Koalas:"));
+// let koalasScore2 = Number(prompt("Enter the 2nd score of Koalas:"));
+// let koalasScore3 = Number(prompt("Enter the 3rd score of Koalas:"));
 
-const averageDolphin = (dolphinScore1 + dolphinScore2 + dolphinScore3 )/3;
-const averageKoalas = (koalasScore1 + koalasScore2 + koalasScore3 )/3;
-if(averageDolphin > 100 || averageKoalas > 100){
+// const averageDolphin = (dolphinScore1 + dolphinScore2 + dolphinScore3 )/3;
+// const averageKoalas = (koalasScore1 + koalasScore2 + koalasScore3 )/3;
+// if(averageDolphin > 100 || averageKoalas > 100){
     
-    if(averageDolphin > averageKoalas){
-        console.log("Dolphin is the winner");
+//     if(averageDolphin > averageKoalas){
+//         console.log("Dolphin is the winner");
         
-    }else if(averageKoalas > averageDolphin){
-        console.log("Koalas is the winner");
-    }else{
-        console.log("Both are draw");
+//     }else if(averageKoalas > averageDolphin){
+//         console.log("Koalas is the winner");
+//     }else{
+//         console.log("Both are draw");
     
-    }
+//     }
+// }
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// BONUS 1
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log('Both win the trophy!');
+} else {
+  console.log('No one wins the trophy 😭');
 }
+
+//////////////////////////////////
+//coding challenge-4
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
