d23f1d4a6c4a51e80d723ccf462c8b177b5f4ada
PraminBudhathoki <pramin2059@gmail.com>
2022-03-11 22:20:13 +0545

    Asynchronous Code learning

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -4,3 +4,16 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// Synchronous Code
+const p = document.querySelector('.p');
+p.textContent = 'My name is Jonas!';
+alert('Text set!');
+
+p.style.color = 'red';
+/////////////////////////////////////
+// Asynchronous Code
+const p = document.querySelector('.p');
+setTimeout(function () {
+  p.textContent = 'My name is Jonas!';
+}, 5000);
+p.style.color = 'red';
