99c0c15d44a91913e7e9e20b3b89fa0692a5ae6c
PraminBudhathoki <pramin2059@gmail.com>
2022-03-12 10:14:30 +0545

    Start it after the exam.Time == very less

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -5,15 +5,16 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 // Synchronous Code
-const p = document.querySelector('.p');
-p.textContent = 'My name is Jonas!';
-alert('Text set!');
+// const p = document.querySelector('.p');
+// p.textContent = 'My name is Jonas!';
+// alert('Text set!');
 
-p.style.color = 'red';
-/////////////////////////////////////
-// Asynchronous Code
-const p = document.querySelector('.p');
-setTimeout(function () {
-  p.textContent = 'My name is Jonas!';
-}, 5000);
-p.style.color = 'red';
+// p.style.color = 'red';
+///////////////////////////////////////
+// // Asynchronous Code
+// const p = document.querySelector('.p');
+// setTimeout(function () {
+//   p.textContent = 'My name is Jonas!';
+// }, 5000);
+// p.style.color = 'red';
+//////////////////////////////////////////////
